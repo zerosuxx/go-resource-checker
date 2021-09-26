7a61d4b55fe3f480af1e02800c880ce353e0cbf5
Tamas Mohos <zerosuxx@gmail.com>
2021-09-27 00:10:01 +0200

    Update checker.go

--- checker.go
+++ checker.go
@@ -12,7 +12,7 @@ import (
 )
 
 func main() {
-	const VERSION = "0.1.0"
+	const VERSION = "0.1.1"
 	log.Println("Res0urce Checker " + VERSION)
 
 	serverCommand := flag.NewFlagSet("server", flag.ContinueOnError)
